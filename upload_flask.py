1790238e4def5bdc16b019f6a8c4f911969b06c1
Sören Hübner <soeren.huebner@bccn-berlin.de>
2020-08-11 14:31:43 +0200

    rearranged the structure of upload_flask and added an empty file for all youtube functionality

--- upload_flask.py
+++ upload_flask.py
@@ -1,39 +1,63 @@
 import os
+from datetime import datetime as dt
 
 from pymongo import MongoClient
-client = MongoClient('localhost', 27017, username='locobln_mongoroot', password='Start.Mongo!')
-db = client.video_database
-collection = db.video_collection
 
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-app = Flask(__name__)
-UPLOAD_FOLDER = 'videos'
 
 @app.route('/upload')
 def upload():
+   # display the upload web page
    return render_template('upload.html')
 	
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
+      # save the uploaded file to the server
       f = request.files['file']
       dst = os.path.join(UPLOAD_FOLDER, secure_filename(f.filename))
       f.save(dst)
       print('video saved')
-      # TODO upload video to youtube
-      # TODO extend the post
-      post = {'title': f.filename}
-      # TODO insert post into database
+
+      # upload video to youtube
+      
+      # construct a document for the mongo db
+      doc = {
+         'title': f.filename,
+         'author': 'author',
+         'location': (33.3, 44.4),
+         'timestamp': dt.timestamp(dt.now()),
+         'type': 'video',
+         'url': 'https://www.youtube.com',
+         'size': 10,
+         }
+      
+      # insert document into database
+      collection.insert_one(doc)
+      
       return 'file uploaded successfully'
 
 @app.route('/')
 def index():
+   # display the homepage
    return render_template('index.html')
 
 		
 if __name__ == '__main__':
-   app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-   app.config['MAX_CONTENT_LENGTH'] = 50 * 10**6
-   app.run(host='0.0.0.0', debug=True)
+   # connect to mongo db
+   client = MongoClient('localhost', 27017, username='locobln_mongoroot', password='Start.Mongo!')
+   db = client.video_database
+   collection = db.video_collection
 
+   # initialise flask
+   app = Flask(__name__)
+
+   # setup the folder where uploaded videos are going to be stored
+   UPLOAD_FOLDER = 'videos'
+   app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+   # constrain max size of uploads to 50MB 
+   app.config['MAX_CONTENT_LENGTH'] = 50 * 10**6
+   
+   # start flask
+   app.run(host='0.0.0.0', debug=True)
